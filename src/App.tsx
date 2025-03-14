b5f53f3a2ca29708b0d46a27099f60d7c8610b0b
Hannah <hannahg.florano@gmail.com>
2025-03-14 19:53:03 +0800

    remove not needed files

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/ui/Navbar";
 function App(){
 
   return (
-    <div className="bg-[url(assets/Moon.svg)] bg-center bg-no-repeat bg-cover bf bg-fixed">
+    <div className="bg-[url(assets/Moon.svg)] bg-center bg-no-repeat bg-cover bg-fixed">
       <Navbar />
       <Header />
       <Main />
