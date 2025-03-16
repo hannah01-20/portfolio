6b987fb595504ee0d022404b9247a38d71422617
Hannah <hannahg.florano@gmail.com>
2025-03-16 14:47:17 +0800

    adjust spacing

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,11 +1,12 @@
 import FlipCard from "../ui/FlipCard";
+import ScrollInfinite from "../ui/ScrollInfinite";
 
 function Skills(){
     return (
-        <section className="min-h-full w-full text-white" id="skills">
-            <div className="w-full h-full bg-gradient-to-b from-black/80 via-white/0 to-black/80 p-8">
+        <section className="sm:h-dvh text-white" id="skills">
+            <div className="w-full h-full bg-gradient-to-b from-black/80 via-white/0 to-black/80 p-8 sm:p-20">
                 <h1 className="text-3xl font-bold ">Skills</h1>
-                <div className="w-full h-full grid justify-center">
+                <div className="w-full h-2/3 grid justify-center">
                     <div className=" h-full py-6 flex flex-col gap-8 lg:gap-12 justify-center items-center md:flex-row md:space-x-2 md:flex-wrap">
                         <FlipCard title="UI/UX Design" description="Prioritizing user experience while achieving feature requirements and modern looking design." />
                         <FlipCard title="Frontend Development" description="Building client-side interface, validating data before sending requests, managing routes, and user state." />
@@ -13,52 +14,12 @@ function Skills(){
                         <FlipCard title="Database Management" description="Managing Relational Database, making queries, add constrains to maintain database structure." />
                     </div>     
                 </div>
-                <div className="w-full mt-10">
+                <div className="w-full mt-10 sm:mt-40 lg:mt-20">
                     <h1 className="text-2xl font-bold text-center relative before:content-[''] before:bg-white before:rounded-full  before:w-1/4 before:h-1 before:absolute before:top-4 before:left-1/2 before:origin-left before:-scale-x-100 before:-translate-x-16
                      after:content-[''] after:bg-white after:rounded-full after:w-1/4 after:h-1 after:absolute after:top-4 after:left-1/2 after:translate-x-16">
                         Tools
                     </h1>
-                    
-                    <div className="h-full overflow-hidden relative">
-                        <div className="w-5/6 mx-auto flex space-x-16 py-16 items-center">
-                            <div className=" flex gap-10 animate-loop-scroll"> 
-                                <i className="devicon-react-original-wordmark colored text-5xl sm:text-8xl"></i>              
-                                <i className="devicon-django-plain-wordmark text-5xl sm:text-8xl"></i>
-                                <i className="devicon-djangorest-line text-5xl sm:text-8xl"></i>
-                                <i className="devicon-mysql-plain-wordmark text-5xl sm:text-8xl"></i>
-                                <i className="devicon-javascript-plain colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-typescript-plain colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-tailwindcss-plain-wordmark colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-sass-original colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-python-plain-wordmark colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-cplusplus-plain colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-vscode-plain-wordmark text-5xl sm:text-8xl"></i>
-                                <i className="devicon-git-plain colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-github-original-wordmark text-5xl sm:text-8xl"></i>
-                                <i className="devicon-azuresqldatabase-plain text-5xl sm:text-8xl"></i>
-                                <i className="devicon-postman-plain colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-figma-plain text-5xl sm:text-8xl"></i>
-                            </div>
-                            <div className="animate-loop-scroll flex gap-10" arial-hidden="true"> 
-                                <i className="devicon-react-original-wordmark colored text-5xl sm:text-8xl"></i>              
-                                <i className="devicon-django-plain-wordmark text-5xl sm:text-8xl"></i>
-                                <i className="devicon-djangorest-line text-5xl sm:text-8xl"></i>
-                                <i className="devicon-mysql-plain-wordmark text-5xl sm:text-8xl"></i>
-                                <i className="devicon-javascript-plain colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-typescript-plain colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-tailwindcss-plain-wordmark colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-sass-original colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-python-plain-wordmark colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-cplusplus-plain colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-vscode-plain-wordmark text-5xl sm:text-8xl"></i>
-                                <i className="devicon-git-plain colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-github-original-wordmark text-5xl sm:text-8xl"></i>
-                                <i className="devicon-azuresqldatabase-plain text-5xl sm:text-8xl"></i>
-                                <i className="devicon-postman-plain colored text-5xl sm:text-8xl"></i>
-                                <i className="devicon-figma-plain text-5xl sm:text-8xl"></i>
-                            </div>
-                        </div>
-                    </div>
+                    <ScrollInfinite />
                 </div>
             </div>
         </section>
---
+++ src/components/ui/ScrollInfinite.tsx
@@ -0,0 +1,47 @@
+function ScrollInfinite() {
+  return (
+    <div className="overflow-hidden">
+        <div className=" flex space-x-16 py-12 items-center">
+            <div className=" flex gap-14 animate-loop-scroll"> 
+                <i className="devicon-react-original-wordmark colored text-4xl sm:text-6xl"></i>              
+                <i className="devicon-django-plain-wordmark text-4xl sm:text-6xl"></i>
+                <i className="devicon-djangorest-line text-4xl sm:text-6xl"></i>
+                <i className="devicon-mysql-plain-wordmark text-4xl sm:text-6xl"></i>
+                <i className="devicon-javascript-plain colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-typescript-plain colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-css3-plain-wordmark text-4xl sm:text-6xl"></i>
+                <i  className="devicon-tailwindcss-plain-wordmark colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-sass-original colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-python-plain-wordmark colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-cplusplus-plain colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-vscode-plain-wordmark text-4xl sm:text-6xl"></i>
+                <i className="devicon-git-plain colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-github-original-wordmark text-4xl sm:text-6xl"></i>
+                <i className="devicon-azuresqldatabase-plain text-4xl sm:text-6xl"></i>
+                <i className="devicon-postman-plain colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-figma-plain text-4xl sm:text-6xl"></i>
+            </div>
+            <div className="animate-loop-scroll flex gap-14" arial-hidden="true"> 
+                <i className="devicon-react-original-wordmark colored text-4xl sm:text-6xl"></i>              
+                <i className="devicon-django-plain-wordmark text-4xl sm:text-6xl"></i>
+                <i className="devicon-djangorest-line text-4xl sm:text-6xl"></i>
+                <i className="devicon-mysql-plain-wordmark text-4xl sm:text-6xl"></i>
+                <i className="devicon-javascript-plain colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-typescript-plain colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-tailwindcss-plain-wordmark colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-sass-original colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-python-plain-wordmark colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-cplusplus-plain colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-vscode-plain-wordmark text-4xl sm:text-6xl"></i>
+                <i className="devicon-git-plain colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-github-original-wordmark text-4xl sm:text-6xl"></i>
+                <i className="devicon-azuresqldatabase-plain text-4xl sm:text-6xl"></i>
+                <i className="devicon-postman-plain colored text-4xl sm:text-6xl"></i>
+                <i className="devicon-figma-plain text-4xl sm:text-6xl"></i>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ScrollInfinite
