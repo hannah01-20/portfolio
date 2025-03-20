b935773048072c9b581f3fa0918782c2eb89cc0d
Hannah <hannahg.florano@gmail.com>
2025-03-20 15:07:04 +0800

    fixed flipcard backface visibility when browsing in firefox

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -1,10 +1,28 @@
 function Experience(){
     return (
-        <section className="h-dvh text-white" id="experience">
+        <section className=" text-white" id="experience">
             <div className="w-full h-full bg-gradient-to-b from-black/80 via-white/0 to-black/50 p-8 md:p-20">
-                <h1 className="text-3xl font-bold">Experience</h1>
-                <div className="w-full h-full flex justify-evenly items-center">  
-
+                <h1 className="text-3xl font-bold mb-10">Experience</h1>
+                <div className="w-full h-full flex justify-evenly">  
+                    <div className="w-full lg:w-4/5">
+                        <div className="grid grid-cols-2 w-full items-center">
+                            <h1 className="text-sm font-semibold sm:text-3xl">Frontend Developer Intern</h1>
+                            <div className="text-xs text-right sm:text-xl">
+                                <p>October 2024 - November 2024</p>
+                                <p>Ollopa Corporation</p>
+                            </div>
+                        </div>
+                        <div>
+                            <ul className="list-disc p-4 text-xs grid gap-2 justify-center sm:text-2xl">
+                                <li>Part of AI Department and frontend team</li>
+                                <li>Part of AI Department and frontend team</li>
+                                <li>Part of AI Department and frontend team</li>
+                                <li>Part of AI Department and frontend team</li>
+                                <li>Part of AI Department and frontend team</li>
+                                
+                            </ul>
+                        </div>
+                    </div>
                 </div>    
             </div>
         </section>
--- src/components/ui/FlipCard.tsx
+++ src/components/ui/FlipCard.tsx
@@ -10,7 +10,7 @@ function FlipCard({ title, description }: FlipCardProps){
     const handleFlip = () => setIsFlipped(e => !e)
     return (
         <div className={`card-container w-[225px] h-[225px] cursor-pointer`}>
-            <div className={`card relative h-full w-full  transform-3d transition duration-500 ease-in-out lg:hover:rotate-y-180 ${isFlipped && "rotate-y-180"}`} 
+            <div className={`card relative h-full w-full  transform-3d transition duration-500 ease-in-out lg:hover:rotate-y-180 ${isFlipped && "rotate-y-180"} backface-hidden`} 
                 onClick={handleFlip}>
                 <div className="card-front absolute h-full w-full backface-hidden bg-blue-200/15 backdrop-blur-lg border border-blue-200 rounded-lg shadow-xl shadow-blue-200 p-3 flex justify-center items-center">
                     <h1 className="text-xl font-bold text-center">{title}</h1>
